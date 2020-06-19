94be522a1a51923fa476805a3b4388a40f560cc5
Ildar Safin <safild94@gmail.com>
2020-06-19 22:51:48 +0200

    Add models

--- edudam/settings.py
+++ edudam/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'edudam.herokuapp.com']
 # Application definition
 
 INSTALLED_APPS = [
+    'tgbot.apps.TgbotConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
