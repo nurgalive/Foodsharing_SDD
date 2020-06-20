f09cf9d59e20ed0ac36d9f6e342538e9114cc368
aleynikovsa <aleynikovsa@yandex-team.ru>
2020-06-20 16:49:07 +0300

    added Category table to admin

--- tgbot/admin.py
+++ tgbot/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import User, Message, Comment, Post, Group
+from .models import User, Message, Comment, Post, Group, Category
 
 admin.site.register(User)
 admin.site.register(Message)
 admin.site.register(Group)
 admin.site.register(Comment)
 admin.site.register(Post)
+admin.site.register(Category)
