7b6defdc6bfab065aebd718accb3ed8284102594
Your Name <azat.nur@gmail.com>
2020-06-20 14:54:30 +0200

    Extended database model for users and categories

--- tgbot/models.py
+++ tgbot/models.py
@@ -6,6 +6,14 @@ class User(models.Model):
   user_id     = models.IntegerField(unique=True, primary_key=True)
   first_name  = models.CharField(max_length=64)
   last_name   = models.CharField(max_length=64)
+  city        = models.CharField(max_length=64)
+
+class Category(models.Model):
+  name = models.TextField(max_length=4096)
+
+class UserToCategory(models.Model):
+  user = models.ForeignKey(Group, on_delete=models.CASCADE, related_name="group", default=0)
+  category = models.ForeignKey(Group, on_delete=models.CASCADE, related_name="group", default=0)
 
   def __str__(self):
       return f'{self.first_name} {self.last_name}'
@@ -20,25 +28,25 @@ class Message(models.Model):
       return f'{self.text}'
 
 class Group(models.Model):
-  group_id= models.IntegerField(unique=True, primary_key=True)
-  name = models.TextField(max_length=4096)
-  link = models.URLField()
-  city = models.TextField(max_length=32)
+  group_id = models.IntegerField(unique=True, primary_key=True)
+  name     = models.TextField(max_length=4096)
+  link     = models.URLField()
+  city     = models.TextField(max_length=32)
 
   def __str__(self):
       return f'{self.name}'
 
 class Post(models.Model):
-  post_id = models.IntegerField(unique=True, primary_key=True)
-  text = models.TextField(max_length=4096, blank=True, null=True)
+  post_id     = models.IntegerField(unique=True, primary_key=True)
+  text        = models.TextField(max_length=4096, blank=True, null=True)
   posted_date = models.DateTimeField(default=timezone.now, null=True, blank=True)
-  link = models.URLField(blank=True, null=True)
-  group_id = models.ForeignKey(Group, on_delete=models.CASCADE, related_name="group", default=0)
-  city = models.TextField(max_length=32, blank=True, null=True)
-  address = models.TextField(max_length=4096, blank=True, null=True)
-  category = models.TextField(max_length=64, blank=True, null=True)
-  is_book = models.BooleanField(default=False)
-  is_lost = models.BooleanField(default=False)
+  link        = models.URLField(blank=True, null=True)
+  group_id    = models.ForeignKey(Group, on_delete=models.CASCADE, related_name="group", default=0)
+  city        = models.TextField(max_length=32, blank=True, null=True)
+  address     = models.TextField(max_length=4096, blank=True, null=True)
+  category    = models.TextField(max_length=64, blank=True, null=True)
+  is_book     = models.BooleanField(default=False)
+  is_lost     = models.BooleanField(default=False)
 
 
 class Comment(models.Model):
