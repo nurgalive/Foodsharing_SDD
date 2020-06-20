529502d691d121dc5fc85b7a5f6da89bf8e67fa5
Ildar Safin <safild94@gmail.com>
2020-06-20 02:52:26 +0200

    Try with echo

--- tgbot/bot.py
+++ tgbot/bot.py
@@ -3,97 +3,21 @@ from django.conf import settings
 import logging
 
 from telegram import Bot as TelegramBot, Update
-from telegram import (ReplyKeyboardMarkup, ReplyKeyboardRemove)
 from telegram.ext import Dispatcher, Updater
-from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters,
-                          ConversationHandler)
 
-# Enable logging
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
+update_id = None
 
-logger = logging.getLogger(__name__)
+def echo(bot):
+  """Echo the message the user sent."""
+  global update_id
+  # Request updates after the last update_id
+  for update in bot.get_updates(offset=update_id, timeout=10):
+      update_id = update.update_id + 1
 
-GENDER, PHOTO, LOCATION, BIO = range(4)
+      if update.message:  # your bot can receive updates without messages
+          # Reply to the message
+          update.message.reply_text(update.message.text)
 
-
-def start(update, context):
-    reply_keyboard = [['Boy', 'Girl', 'Other']]
-
-    update.message.reply_text(
-        'Hi! My name is Professor Bot. I will hold a conversation with you. '
-        'Send /cancel to stop talking to me.\n\n'
-        'Are you a boy or a girl?',
-        reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
-
-    return GENDER
-
-
-def gender(update, context):
-    user = update.message.from_user
-    logger.info("Gender of %s: %s", user.first_name, update.message.text)
-    update.message.reply_text('I see! Please send me a photo of yourself, '
-                              'so I know what you look like, or send /skip if you don\'t want to.',
-                              reply_markup=ReplyKeyboardRemove())
-
-    return PHOTO
-
-
-def photo(update, context):
-    user = update.message.from_user
-    photo_file = update.message.photo[-1].get_file()
-    photo_file.download('user_photo.jpg')
-    logger.info("Photo of %s: %s", user.first_name, 'user_photo.jpg')
-    update.message.reply_text('Gorgeous! Now, send me your location please, '
-                              'or send /skip if you don\'t want to.')
-
-    return LOCATION
-
-
-def skip_photo(update, context):
-    user = update.message.from_user
-    logger.info("User %s did not send a photo.", user.first_name)
-    update.message.reply_text('I bet you look great! Now, send me your location please, '
-                              'or send /skip.')
-
-    return LOCATION
-
-
-def location(update, context):
-    user = update.message.from_user
-    user_location = update.message.location
-    logger.info("Location of %s: %f / %f", user.first_name, user_location.latitude,
-                user_location.longitude)
-    update.message.reply_text('Maybe I can visit you sometime! '
-                              'At last, tell me something about yourself.')
-
-    return BIO
-
-
-def skip_location(update, context):
-    user = update.message.from_user
-    logger.info("User %s did not send a location.", user.first_name)
-    update.message.reply_text('You seem a bit paranoid! '
-                              'At last, tell me something about yourself.')
-
-    return BIO
-
-
-def bio(update, context):
-    user = update.message.from_user
-    logger.info("Bio of %s: %s", user.first_name, update.message.text)
-    update.message.reply_text('Thank you! I hope we can talk again some day.')
-
-    return ConversationHandler.END
-
-
-def cancel(update, context):
-    user = update.message.from_user
-    logger.info("User %s canceled the conversation.", user.first_name)
-    update.message.reply_text('Bye! I hope we can talk again some day.',
-                              reply_markup=ReplyKeyboardRemove())
-
-    return ConversationHandler.END
 
 
 class Bot:
@@ -110,25 +34,19 @@ class Bot:
       self.bot.set_webhook('{}/{}/{}/'.format(url, 'bot', token))
 
       self.dispatcher = Dispatcher(self.bot, None, workers=0)
-      conv_handler = ConversationHandler(
-          entry_points=[CommandHandler('start', start)],
 
-          states={
-              GENDER: [MessageHandler(Filters.regex('^(Boy|Girl|Other)$'), gender)],
+      global update_id
 
-              PHOTO: [MessageHandler(Filters.photo, photo),
-                      CommandHandler('skip', skip_photo)],
+      # get the first pending update_id, this is so we can skip over it in case
+      # we get an "Unauthorized" exception.
+      try:
+          update_id = self.bot.get_updates()[0].update_id
+      except IndexError:
+          update_id = None
 
-              LOCATION: [MessageHandler(Filters.location, location),
-                        CommandHandler('skip', skip_location)],
-
-              BIO: [MessageHandler(Filters.text, bio)]
-          },
-
-          fallbacks=[CommandHandler('cancel', cancel)]
-      )
-
-      self.dispatcher.add_handler(conv_handler)
+      logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+      
+      echo(self.bot)
 
     def register(self, handler):
         handler.register(self.dispatcher)
