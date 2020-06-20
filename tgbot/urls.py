10560e135e3e8a6268765273473bf8642ff1695c
Ildar Safin <safild94@gmail.com>
2020-06-20 02:16:19 +0200

    Fix

--- tgbot/urls.py
+++ tgbot/urls.py
@@ -1,7 +1,7 @@
 from django.urls import re_path
 from . import views
 
-from .views import webhook
+from .views import webhook_message
 
 urlpatterns = [
   re_path(r'webhook/(?P<token>.+)/', webhook_message),
