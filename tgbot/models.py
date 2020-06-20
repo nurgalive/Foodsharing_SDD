9d1d5aff21ef1b13951bf78a6f602bb038a1245a
Ildar Safin <safild94@gmail.com>
2020-06-20 21:53:32 +0200

    Fix model

--- tgbot/models.py
+++ tgbot/models.py
@@ -8,15 +8,15 @@ class User(models.Model):
   last_name   = models.CharField(max_length=64, blank=True, null=True)
   city        = models.CharField(max_length=64, blank=True, null=True)
 
+  def __str__(self):
+    return f'{self.first_name} {self.last_name}'
+
 class Category(models.Model):
   name = models.TextField(max_length=4096)
 
 class UserToCategory(models.Model):
   user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="group", default=0)
   category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="group", default=0)
-
-  def __str__(self):
-      return f'{self.first_name} {self.last_name}'
 
 class Message(models.Model):
   update_id = models.IntegerField(unique=True)
