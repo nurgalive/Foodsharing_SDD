bfe5ec3d847d959b676e12bcea45a8cd9a88263f
Ildar Safin <safild94@gmail.com>
2020-06-20 14:47:22 +0200

    Move methods to init stage

--- tgbot/bot.py
+++ tgbot/bot.py
@@ -20,38 +20,6 @@ logger = logging.getLogger(__name__)
 
 CATEGORIES = range(1)
 
-
-def start(update, context):
-  reply_keyboard = [['Москва', 'Спб']]
-
-  update.message.reply_text(
-    'Привет! Это бот едудам. Поговори со мной. '
-    'Отправь /cancel чтобы остановить разговор со мной.\n\n'
-    'Выбери город',
-    reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
-
-  return CATEGORIES
-
-
-def categories(update, context):
-  reply_keyboard = [['Все', 'Молоко', "Хлеб"]]
-  user = update.message.from_user
-
-  update.message.reply_text(
-    'Выбери интересующие категории продуктов',
-    reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
-
-  return ConversationHandler.END
-
-
-def cancel(update, context):
-  user = update.message.from_user
-  logger.info("User %s canceled the conversation.", user.first_name)
-  update.message.reply_text('Bye! I hope we can talk again some day.',
-                            reply_markup=ReplyKeyboardRemove())
-
-  return ConversationHandler.END
-
 class Bot:
   def __init__(self, token, url='edudam.herokuapp.com'):
     self.bot = TelegramBot(token)
@@ -66,24 +34,57 @@ class Bot:
     self.bot.set_webhook('{}/{}/{}/'.format(url, 'bot', token))
 
     self.dispatcher = Dispatcher(self.bot, None, workers=0)
-    
+
+    self.update_obj = None
+
+    # Add conversation handler with the states GENDER, PHOTO, LOCATION and BIO
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler('start', self.start)],
+
+        states={
+          CATEGORIES: [MessageHandler(Filters.regex('^(Boy|Girl|Other)$'), self.categories)],
+        },
+
+        fallbacks=[CommandHandler('cancel', self.cancel)]
+    )
+
+    self.dispatcher.add_handler(conv_handler)
+
+
+  def start(self, context):
+    reply_keyboard = [['Москва', 'Спб']]
+
+    self.update_obj.message.reply_text(
+      'Привет! Это бот едудам. Поговори со мной. '
+      'Отправь /cancel чтобы остановить разговор со мной.\n\n'
+      'Выбери город',
+      reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
+
+    return CATEGORIES
+
+
+  def categories(self, context):
+    reply_keyboard = [['Все', 'Молоко', "Хлеб"]]
+    user = self.update_obj.message.from_user
+
+    self.update_obj.message.reply_text(
+      'Выбери интересующие категории продуктов',
+      reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
+
+    return ConversationHandler.END
+
+
+  def cancel(self, context):
+    user = self.update_obj.message.from_user
+    logger.info("User %s canceled the conversation.", user.first_name)
+    self.update_obj.message.reply_text('Bye! I hope we can talk again some day.',
+                              reply_markup=ReplyKeyboardRemove())
+
+    return ConversationHandler.END
 
   def register(self, handler):
     handler.register(self.dispatcher)
 
   def webhook(self, update):
-    update_obj = Update.de_json(update, self.bot)
-
-    # Add conversation handler with the states GENDER, PHOTO, LOCATION and BIO
-    conv_handler = ConversationHandler(
-        entry_points=[CommandHandler('start', start(update_obj, self.bot))],
-
-        states={
-          CATEGORIES: [MessageHandler(Filters.regex('^(Boy|Girl|Other)$'), categories(update_obj, self.bot))],
-        },
-
-        fallbacks=[CommandHandler('cancel', cancel(update_obj, self.bot))]
-    )
-
-    self.dispatcher.add_handler(conv_handler)
-    self.dispatcher.process_update(update_obj)
+    self.update_obj = Update.de_json(update, self.bot)
+    self.dispatcher.process_update(self.update_obj)
