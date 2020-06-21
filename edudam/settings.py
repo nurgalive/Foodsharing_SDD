7bec850e5161cb3ad50b8089cd299d61dad73980
Your Name <azat.nur@gmail.com>
2020-06-21 18:06:51 +0200

    Added persisted bd connection settings

--- edudam/settings.py
+++ edudam/settings.py
@@ -134,3 +134,10 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 django_heroku.settings(locals())
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
