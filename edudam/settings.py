5641d48071a8ca0a8c8ff11e5c587da135ced45f
Your Name <azat.nur@gmail.com>
2020-09-26 13:01:58 +0200

    Changed env variables

--- edudam/settings.py
+++ edudam/settings.py
@@ -142,3 +142,8 @@ DATABASES['default'].update(db_from_env)
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# Environmental variables
+VK_LOGIN = os.environ['VK_LOGIN']
+VK_PASS = os.environ['VK_PASS']
+BOT_TOKEN = os.environ['BOT_TOKEN']
